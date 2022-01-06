f083e060624a40cc45bef472a78918715b39b97b
Vinay-Kushwaha <kushwaha122@gmail.com>
2022-01-06 05:39:50 +0530

    Navigation

--- frontend/src/components/shared/Navigation/Navigation.jsx
+++ frontend/src/components/shared/Navigation/Navigation.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import {Link } from 'react-router-dom';
 
+import styles from './Navigation.module.css'
+
 export default function Navigation() {
+    //javaScript doesn't allow '-' hence we use camelCase to Apply classes.
+    const brandStyle={
+        color:'fff',
+        textDecoration:'none',
+        fontWeight:'bold',
+        fontSize:'18px',
+        display:'flex',
+        alignItems:'center'
+    }
+
+    const logoText={
+        marginLeft:'10px',
+    }
+
     return (
-        <nav>
-            <h1>This is Navigatioon</h1>
-            <Link to="/">
+        <nav className={`${styles.navbar} container`}>
+            <Link style={brandStyle} to="/">
                 <img src="./images/hand-icon.png" alt="logo" />
-                <span>Coders House</span>
+                <span style={logoText}>Coders House</span>
             </Link>
         </nav>
     )
